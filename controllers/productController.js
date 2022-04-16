26172c557629e23705767bd9216bfb54cd52adb3
ValenFP <vflorespueyo@udesa.edu.ar>
2022-04-15 22:21:35 -0300

    Catalogo

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,17 +12,23 @@ const controlador= {
     add : (req,res)=>{
         res.render("product-add")
     },
-    products: function(req, res) {
-        if (Object.keys(req.query).length !== 0) return res.send(drinks.findDrinksBy(req.query));
-        res.render('products', { drinks: drinks.getAll() });
-    },
-    author: function(req, res) {
-        const result = drinks.findDrinksBy({'title': req.params.title});
-        res.render('drinks_index', { drinks: result });
-    },
-    show: function(req, res) {
-        res.render('drinks_show', { drinks: drinks.findDrinksById(req.params.id)});
+    products: function (req, res) {
+        res.render('products', {drinks: data.drinks});
     }
+    // products: function(req, res) {
+    //     if (Object.keys(req.query).length !== 0) return res.send(drinks.findDrinksBy(req.query));
+    //     res.render('products', { drinks: drinks.getAll() });
+    // },
+    // author: function(req, res) {
+    //     const result = drinks.findDrinksBy({'title': req.params.title});
+    //     res.render('drinks_index', { drinks: result });
+    // },
+    // show: function(req, res) {
+    //     res.render('drinks_show', { drinks: drinks.findDrinksById(req.params.id)});
+    // },
+    // showImage: function(req, res) {
+    //     res.render('products', {drinks: data.drinks});
+    // }
 };
 
 module.exports = controlador;
