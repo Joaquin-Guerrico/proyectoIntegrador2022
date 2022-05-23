07d33057420212518602567855d07520f83c20a4
gero-galvez <ggalvez@udesa.edu.ar>
2022-05-23 14:57:33 -0300

    sequelize

--- db/data.js
+++ db/data.js
@@ -7,16 +7,16 @@ const data = {
   }
   ,
     drinks:[
-    {id: "1", title: "Corona", description: "Cerveza ", "price": "$200", cover: "/images/products/product1.jpg"},
-    {id: "2", title: "Fernet Branca ", description: "Fernet", "price":  "$999", cover: "/images/products/product2.jpg"},
-    {id: "3", title: "Beefeater London ", description: "Dry Gin","price":  "$2.300",cover: "/images/products/product3.jpg"},
-    {id: "4", title: "Heneiken ", description: "Cerveza", "price":  "$250",cover: "/images/products/product4.jpg"},
-    {id: "5", title: "Jagermeiter", description: "Licor de Hierbas", "price":  "$3.000",cover: "/images/products/product5.jpg"},
-    {id: "6", title: "Jhonie Walker", description: "Jhonie Walker Black Label, Whiskey", "price": "$5.000",cover: "/images/products/product6.jpg"},
-    {id: "7", title: "Havana Club", description: "Ron",   "price":  "$1.500",cover: "/images/products/product7.jpg"},
-    {id: "8", title: "Schneider", description: "Cerveza",  "price":  "$200",cover: "/images/products/product8.jpg"},
-    {id: "9", title: "Jose Cuervo ", description: "Tequila",   "price":  "$3.000",cover: "/images/products/product9.jpg"},
-    {id: "10", title: "Smirnoff", description: "Vodka",   "price":  "$850",cover: "/images/products/product10.jpg"}
+    {id: "1", title: "Corona", description: "Cerveza ", price: "$200", cover: "/images/products/product1.jpg"},
+    {id: "2", title: "Fernet Branca ", description: "Fernet", price:  "$999", cover: "/images/products/product2.jpg"},
+    {id: "3", title: "Beefeater London ", description: "Dry Gin",price:  "$2.300",cover: "/images/products/product3.jpg"},
+    {id: "4", title: "Heneiken ", description: "Cerveza", price:  "$250",cover: "/images/products/product4.jpg"},
+    {id: "5", title: "Jagermeiter", description: "Licor de Hierbas", price:  "$3.000",cover: "/images/products/product5.jpg"},
+    {id: "6", title: "Jhonie Walker", description: "Jhonie Walker Black Label, Whiskey", price: "$5.000",cover: "/images/products/product6.jpg"},
+    {id: "7", title: "Havana Club", description: "Ron",   price:  "$1.500",cover: "/images/products/product7.jpg"},
+    {id: "8", title: "Schneider", description: "Cerveza",  price:  "$200",cover: "/images/products/product8.jpg"},
+    {id: "9", title: "Jose Cuervo ", description: "Tequila",   price:  "$3.000",cover: "/images/products/product9.jpg"},
+    {id: "10", title: "Smirnoff", description: "Vodka",   price:  "$850",cover: "/images/products/product10.jpg"}
 
   ],
   coments: [
@@ -32,7 +32,7 @@ const data = {
     },
     {
       username:"El Tucu Correa" ,
-      coment:"	¡Nada como tomar un buen fernet durante un asasdo con amigos! "  ,
+      coment:"	¡Nada como tomar un buen fernet durante un asado con amigos! "  ,
       picture:"/images/users/user3.jpg" ,
     },
     {
