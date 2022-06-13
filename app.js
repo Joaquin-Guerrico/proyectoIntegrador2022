20d5f02e77c015200d9a1ec79ab0109dbb1dc65f
gero-galvez <ggalvez@udesa.edu.ar>
2022-06-13 13:55:33 -0300

    n

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 
+var session = require('express-session');
 var app = express();
 
 // view engine setup
@@ -24,7 +25,11 @@ app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/profile', usersRouter);
 
-
+app.use(session({
+  secret: "nuestro mensaje secreto",
+  resave: false,
+  saveUninitialized: true,
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
